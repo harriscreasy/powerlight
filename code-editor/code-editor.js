bf8be1a993399293f33966aaed285b591b3c9dd9
harriscreasy <harrisbzaoui@gmail.com>
2021-12-20 12:53:21 -0500

    Optimisation

--- code-editor/code-editor.js
+++
@@ -1,9 +0,0 @@
-function showPreview(){
-    let htmlCode = document.getElementById("htmlCode").value;
-    let cssCode = "<style>"+document.getElementById("cssCode").value+"</style>";
-    let jsCode = "<scri"+"pt>"+document.getElementById("jsCode").value+"</scri"+"pt>";
-    var frame = document.querySelector("#preview-window").contentWindow.document;
-    frame.open();
-    frame.write(htmlCode+cssCode+jsCode);
-    frame.close();
-}
